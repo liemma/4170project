23bcb4008bb2efe7f2be2f837ca07305e3616b03
Anna Sun <145409229+annasun10@users.noreply.github.com>
2025-04-20 18:26:53 -0400

    updated website layout

--- server.py
+++ server.py
@@ -24,31 +24,14 @@ def song_list():
 def practice():
     return render_template("practice.html")
 
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html")
+
 
 # AJAX FUNCTIONS
 
-# ajax for people.js
-@app.route('/add_name', methods=['GET', 'POST'])
-def add_name():
-    global data 
-    global current_id 
 
-    json_data = request.get_json()   
-    name = json_data["name"] 
-    
-    # add new entry to array with 
-    # a new id and the name the user sent in JSON
-    current_id += 1
-    new_id = current_id 
-    new_name_entry = {
-        "name": name,
-        "id":  current_id
-    }
-    data.append(new_name_entry)
-
-    #send back the WHOLE array of data, so the client can redisplay it
-    return jsonify(data = data)
- 
 
 
 if __name__ == '__main__':
