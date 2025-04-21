18f5cb7e94afc8ff14b7cdab891c825f8850702d
liemma <57811437+liemma@users.noreply.github.com>
2025-04-21 18:30:14 -0400

    added learn song, song list, implemented quiz

--- server.py
+++ server.py
@@ -11,6 +11,30 @@ quiz_questions = [
     {"image": "type_note_2.png", "answer": "quarter note"},
 ]
 
+song_list = {
+    "1": {
+        "title": "Mary Had a Little Lamb",
+        "image": "images/mary_lamb.png",
+        "measures": [
+            "images/mary_measures_1.png",
+            "images/mary_measures_2.png",
+            "images/mary_measures_3.png",
+            "images/mary_measures_4.png"
+        ]
+    },
+    "2": {
+        "title": "Happy Birthday",
+        "image": "images/happy_birthday.png",
+        "measures": [
+            "images/happy_measures_1.png",
+            "images/happy_measures_2.png",
+            "images/happy_measures_3.png"
+        ],
+        "description": "It’s a great piece to learn for beginners. Fun fact: This is also the tune of Twinkle Twinkle Little Star",
+        "song_notes": "The primary notes in this song are E, D, C, G",
+    }
+}
+
 # ROUTES
 
 @app.route('/')
@@ -25,9 +49,30 @@ def piano_basics():
 def song_list():
     return render_template("song_list.html")
 
+# have argument
+@app.route('/learn_song')
+def learn_song():
+    song_param = request.args.get("song")
+
+    song_title = {
+        "mary_lamb": "Mary Had a Little Lamb",
+        "happy_birthday": "Happy Birthday"
+    }.get(song_param)
+
+    return render_template("learn_song.html", song_title=song_title)
+
+
 @app.route('/practice')
 def practice():
-    return render_template("practice.html")
+    song_param = request.args.get("song")
+
+    song_title = {
+        "happy_birthday": "Happy Birthday",
+        "mary_lamb": "Mary Had a Little Lamb"
+    }.get(song_param, None)
+
+    return render_template("practice.html", song_title=song_title)
+
 
 @app.route("/quiz", methods=["GET", "POST"])
 def quiz():
